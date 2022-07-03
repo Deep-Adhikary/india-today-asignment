f2072d0c48aebe7a624c39a80bdb6f06bd550980
Deep <deep.adhikary@outlook.com>
2022-07-04 00:25:11 +0530

    Test Script Design In progress
    - Modified test 'launchAppandChangeLanguage'
    - Ceated test 'verifyPageTheme'

--- tak/src/test/java/assignment/indiatoday/SportsTakTest.java
+++ tak/src/test/java/assignment/indiatoday/SportsTakTest.java
@@ -3,6 +3,7 @@ package assignment.indiatoday;
 import org.testng.annotations.*;
 
 import assignment.indiatoday.base.factories.WebDriverFactory;
+import assignment.indiatoday.base.pages.Home;
 import assignment.indiatoday.base.utils.Browser;
 import assignment.indiatoday.base.utils.PropertyReader;
 
@@ -14,22 +15,47 @@ public class SportsTakTest {
      * Rigorous Test :-)
      */
     private WebDriver driver;
+
+    @SuppressWarnings("unused")
     private PropertyReader reader;
+
+    private Home homepage;
 
     @BeforeClass
     public void setUp() throws Exception {
-        driver = WebDriverFactory.getDriver(Browser.CHROME);
+        driver = WebDriverFactory.getDriver(Browser.FIREFOX);
         reader = PropertyReader.getInstance();
+        homepage = new Home(driver);
     }
 
     @Test
-    public void launchAppandSelectLanguage() {
-        driver.navigate().to(reader.get("appurl"));
+    public void launchAppandChangeLanguage() {
+        homepage.navigate();
+        homepage.getLeftMenu().open();
+        homepage.getLeftMenu().changeLanguage("hindi");
+        homepage.verifyLanguageIsSetTo("hindi");
+        homepage.getLeftMenu().open();
+        homepage.getLeftMenu().refreshePageObjects();
+        homepage.getLeftMenu().changeLanguage("english");
+
+    }
+
+    @Test
+    void verifyPageTheme() {
+        homepage.navigate();
+        homepage.getLeftMenu().open();
+        homepage.getLeftMenu().changeTheme();
+        homepage.verifyPageThemeIs("white");
+        homepage.getLeftMenu().refreshePageObjects();
+        homepage.getLeftMenu().open();
+        homepage.getLeftMenu().changeTheme();
+        homepage.verifyPageThemeIs("black");
 
     }
 
     @AfterClass
     public void tearDown() {
+        driver.close();
         driver.quit();
     }
 }
