b9f44d7320d112a47c6d14b54f3eac293c9029f3
Deep <deep.adhikary@outlook.com>
2022-07-05 00:55:12 +0530

    Custom component created

---
+++ tak/src/test/java/assignment/indiatoday/base/utils/CustomCapabilities.java
@@ -0,0 +1,27 @@
+package assignment.indiatoday.base.utils;
+
+import java.util.List;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CustomCapabilities {
+    private WebDriver driver;
+
+    public CustomCapabilities(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement expandShadowRootElement(WebElement element) {
+        @SuppressWarnings("unchecked")
+        List<WebElement> eles = (List<WebElement>) ((JavascriptExecutor) driver)
+                .executeScript("return arguments[0].shadowRoot.children", element);
+        return eles.get(0);
+    }
+
+    public void clickUsingJavaScript(WebElement element) {
+        ((JavascriptExecutor) driver)
+                .executeScript("arguments[0].click();", element);
+    }
+}
