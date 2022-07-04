d6edd62183fd257cfcf9e011afd045df63ac03dd
Deep <deep.adhikary@outlook.com>
2022-07-05 00:54:12 +0530

    Top Navigation Object Introduced

--- tak/src/test/java/assignment/indiatoday/base/BasePage.java
+++ tak/src/test/java/assignment/indiatoday/base/BasePage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import assignment.indiatoday.base.pages.LeftMenu;
+import assignment.indiatoday.base.pages.TopNavigation;
+import assignment.indiatoday.base.utils.CustomCapabilities;
 import assignment.indiatoday.base.utils.PropertyReader;
 
 import org.openqa.selenium.support.PageFactory;
@@ -14,23 +16,38 @@ public class BasePage {
     protected WebDriver driver;
     protected WebDriverWait wait;
     protected PropertyReader reader;
+    protected CustomCapabilities customcapabilities;
     private LeftMenu leftMenu;
+    private TopNavigation topNavigation;
+
     public BasePage(WebDriver driver) {
         reader = PropertyReader.getInstance();
         this.driver = driver;
         wait = new WebDriverWait(this.driver, Duration.ofSeconds(30));
+        customcapabilities = new CustomCapabilities(driver);
         PageFactory.initElements(this.driver, this);
     }
-    public void refreshePageObjects(){
+
+    public void refreshePageObjects() {
         PageFactory.initElements(this.driver, this);
     }
-    public LeftMenu getLeftMenu(){
-        if (leftMenu!=null){
+
+    public LeftMenu getLeftMenu() {
+        if (leftMenu != null) {
             return leftMenu;
         }
-        leftMenu=new LeftMenu(driver);
+        leftMenu = new LeftMenu(driver);
         return leftMenu;
     }
+
+    public TopNavigation getTopNavigation() {
+        if (topNavigation != null) {
+            return topNavigation;
+        }
+        topNavigation = new TopNavigation(driver);
+        return topNavigation;
+    }
+
     public void sleep(int milliseconds) {
         try {
             Thread.sleep(milliseconds);
